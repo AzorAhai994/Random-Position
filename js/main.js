3cb2ed6c7f7e93ad3385baf3cf89da2b11e175d5
Marinko Brdarević <39567693+AzorAhai994@users.noreply.github.com>
2019-02-07 16:26:46 +0100

    00

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-var n = parseInt(prompt("Koliko krugova?")); //replace with input
+var n = parseInt(prompt("Koliko krugova?"));
+// replace with input
 
 for (let i = 0; i < n; i++) {
   $(".main-box").append("<div></div>");
-} //div maker
+}
+// div maker
 
 $("body").on("click", function() {
   $(".main-box > div").each(function() {
@@ -22,7 +24,8 @@ $("body").on("click", function() {
     var color = randomColor({
       luminosity: "light",
       hue: "random"
-    }); // a hex code for an attractive color
+    });
+    // a hex code for an attractive color
 
     //var scale = Math.floor(Math.random() * $t.width());
 
@@ -30,60 +33,60 @@ $("body").on("click", function() {
     var randomLeftPos = Math.floor(Math.random() * (bodyWidth - $t.height()));
 
     /*   var randomTopPos = Math.floor(
-          Math.random() * ($(".main-box").height() - $t.width())
-        );
-        var randomTopPos = Math.floor(
-          Math.random() * ($(".main-box").widtht() - $t.height())
-        ); */
+      Math.random() * ($(".main-box").height() - $t.width())
+    );
+    var randomTopPos = Math.floor(
+      Math.random() * ($(".main-box").widtht() - $t.height())
+    ); */
     // alert(contHeight);
 
     /* $(function() {
-      var positions = [];
-      $(".main-box > div").each(function() {
-        var ctx = $(this);
-        var dim = {
-          width: ctx.outerWidth(),
-          height: ctx.outerHeight()
-        };
-        var success = false;
-        while (!success) {
-          $("#log").append(".");
-          dim.left = parseInt(Math.random() * 300);
-          dim.top = parseInt(Math.random() * 300);
-          var success = true;
-          $.each(positions, function() {
-            if (
-              dim.left < this.left + this.width &&
-              dim.left + dim.width > this.left &&
-              dim.top < this.top + this.height &&
-              dim.top + dim.height > this.top
-            ) {
-              success = false;
-            }
-          });
+  var positions = [];
+  $(".main-box > div").each(function() {
+    var ctx = $(this);
+    var dim = {
+      width: ctx.outerWidth(),
+      height: ctx.outerHeight()
+    };
+    var success = false;
+    while (!success) {
+      $("#log").append(".");
+      dim.left = parseInt(Math.random() * 300);
+      dim.top = parseInt(Math.random() * 300);
+      var success = true;
+      $.each(positions, function() {
+        if (
+          dim.left < this.left + this.width &&
+          dim.left + dim.width > this.left &&
+          dim.top < this.top + this.height &&
+          dim.top + dim.height > this.top
+        ) {
+          success = false;
         }
-        $("#log").append(
-          "<div>" + ctx.text() + ":" + dim.left + "," + dim.top + "</div>"
-        );
-        positions.push(dim);
-        ctx.animate(
-          {
-            left: dim.left,
-            top: dim.top
-          },
-          "slow"
-        );
       });
-    }); */
+    }
+    $("#log").append(
+      "<div>" + ctx.text() + ":" + dim.left + "," + dim.top + "</div>"
+    );
+    positions.push(dim);
+    ctx.animate(
+      {
+        left: dim.left,
+        top: dim.top
+      },
+      "slow"
+    );
+  });
+}); */
 
     $t.css({
       top: randomTopPos,
       left: randomLeftPos,
 
-      background: /* "url(" + link + ")" */ color,
+      /* "url(" + link + ")" */
+      background: color,
       width: size,
-      height: size
-      //transform: "scale(" + scale + ")"
+      height: size //transform: "scale(" + scale + ")"
     });
   });
 });
