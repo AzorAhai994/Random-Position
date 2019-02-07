54fc107c6228fd9a03f5778cba0b948606c72d25
Marinko Brdarević <39567693+AzorAhai994@users.noreply.github.com>
2019-02-07 12:45:40 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-var n = parseInt(prompt("Koliko krugova?"));
+var n = parseInt(prompt("Koliko krugova?")); //replace with input
 
 for (let i = 0; i < n; i++) {
   $(".main-box").append("<div></div>");
-}
+} //div maker
+
 $("body").on("click", function() {
   $(".main-box > div").each(function() {
     var $t = $(this);
@@ -22,6 +23,7 @@ $("body").on("click", function() {
       luminosity: "light",
       hue: "random"
     }); // a hex code for an attractive color
+
     //var scale = Math.floor(Math.random() * $t.width());
 
     var randomTopPos = Math.floor(Math.random() * (bodyHeight - $t.width()));
@@ -34,6 +36,46 @@ $("body").on("click", function() {
           Math.random() * ($(".main-box").widtht() - $t.height())
         ); */
     // alert(contHeight);
+
+    /* $(function() {
+      var positions = [];
+      $(".main-box > div").each(function() {
+        var ctx = $(this);
+        var dim = {
+          width: ctx.outerWidth(),
+          height: ctx.outerHeight()
+        };
+        var success = false;
+        while (!success) {
+          $("#log").append(".");
+          dim.left = parseInt(Math.random() * 300);
+          dim.top = parseInt(Math.random() * 300);
+          var success = true;
+          $.each(positions, function() {
+            if (
+              dim.left < this.left + this.width &&
+              dim.left + dim.width > this.left &&
+              dim.top < this.top + this.height &&
+              dim.top + dim.height > this.top
+            ) {
+              success = false;
+            }
+          });
+        }
+        $("#log").append(
+          "<div>" + ctx.text() + ":" + dim.left + "," + dim.top + "</div>"
+        );
+        positions.push(dim);
+        ctx.animate(
+          {
+            left: dim.left,
+            top: dim.top
+          },
+          "slow"
+        );
+      });
+    }); */
+
     $t.css({
       top: randomTopPos,
       left: randomLeftPos,
