eaec620bec5d76f845d174df3a3871c5ce361de9
AzorAhai994 <39567693+AzorAhai994@users.noreply.github.com>
2019-02-10 13:00:01 +0100

    6

--- js/main.js
+++ js/main.js
@@ -1,13 +1,81 @@
-var n = parseInt(prompt("Koliko krugova?"));
-// replace with input
+function divMaker() {
 
-for (let i = 0; i < n; i++) {
-  $(".main-box").append("<div></div>");
+  var n = parseInt(prompt("Koliko krugova?"));
+  // replace with input
+
+  /*  for (let i = 0; i < n; i++) {
+     $(".main-box").append("<div></div>");
+   } */
+  // div maker
+
+
+
+  var a = [];
+  for (var i = 0; i < n; i++) {
+    a.push("<div></div>");
+    console.log("div#" + n);
+  }
+  $(".main-box").append(a.join(""));
 }
-// div maker
 
-$("body").on("click", function() {
-  $(".main-box > div").each(function() {
+
+function manipulations() {
+
+  $(window).on("load", function () {
+
+
+
+    $(".main-box > div").each(function () {
+      var $t = $(this);
+      var bodyWidth = window.innerWidth;
+      var bodyHeight = window.innerHeight;
+      //var link = "https://picsum.photos/35/35?image=" + n;
+
+      var link = "https://placeimg.com/40/40/any";
+
+      var size = Math.floor(Math.random() * (40 - 8 + 1)) + 8;
+      var zIndex = Math.floor(Math.random() * (10 - (-2) + 1)) + (-2);
+
+      if (zIndex < 1) {
+        var blur = 10 + 'px';
+      } else if (zIndex > 1) {
+        var blur = 0 + 'px';
+      } // add blur to z-index
+      var contWidth = window.innerWidth;
+      var contHeight = window.innerHeight;
+
+      // var color = "#" + ((Math.random() * 0xffffff) << 0).toString(16);
+      var color = randomColor({
+        luminosity: "light",
+        hue: "random"
+      });
+      // a hex code for an attractive color
+
+      //var scale = Math.floor(Math.random() * $t.width());
+
+      var randomTopPos = Math.floor(Math.random() * (bodyHeight - $t.width()));
+      var randomLeftPos = Math.floor(Math.random() * (bodyWidth - $t.height()));
+
+
+
+      $t.css({
+        top: randomTopPos,
+        left: randomLeftPos,
+        // filter: "blur(" + blur / zIndex + "px)",
+        /* "url(" + link + ")" */
+        background: color,
+        width: size,
+        height: size,
+        //transform: "scale(" + 1.5 + ")",
+        zIndex: zIndex
+      }); // assign all values to css
+    });
+  });
+
+}
+
+$("body").on('click', function () {
+  $(".main-box > div").each(function () {
     var $t = $(this);
     var bodyWidth = window.innerWidth;
     var bodyHeight = window.innerHeight;
@@ -16,7 +84,13 @@ $("body").on("click", function() {
     var link = "https://placeimg.com/40/40/any";
 
     var size = Math.floor(Math.random() * (40 - 8 + 1)) + 8;
+    var zIndex = Math.floor(Math.random() * (10 - (-2) + 1)) + (-2);
 
+    if (zIndex < 1) {
+      var blur = 10 + 'px';
+    } else if (zIndex > 1) {
+      var blur = 0 + 'px';
+    } // add blur to z-index
     var contWidth = window.innerWidth;
     var contHeight = window.innerHeight;
 
@@ -32,61 +106,22 @@ $("body").on("click", function() {
     var randomTopPos = Math.floor(Math.random() * (bodyHeight - $t.width()));
     var randomLeftPos = Math.floor(Math.random() * (bodyWidth - $t.height()));
 
-    /*   var randomTopPos = Math.floor(
-      Math.random() * ($(".main-box").height() - $t.width())
-    );
-    var randomTopPos = Math.floor(
-      Math.random() * ($(".main-box").widtht() - $t.height())
-    ); */
-    // alert(contHeight);
 
-    /* $(function() {
-  var positions = [];
-  $(".main-box > div").each(function() {
-    var ctx = $(this);
-    var dim = {
-      width: ctx.outerWidth(),
-      height: ctx.outerHeight()
-    };
-    var success = false;
-    while (!success) {
-      $("#log").append(".");
-      dim.left = parseInt(Math.random() * 300);
-      dim.top = parseInt(Math.random() * 300);
-      var success = true;
-      $.each(positions, function() {
-        if (
-          dim.left < this.left + this.width &&
-          dim.left + dim.width > this.left &&
-          dim.top < this.top + this.height &&
-          dim.top + dim.height > this.top
-        ) {
-          success = false;
-        }
-      });
-    }
-    $("#log").append(
-      "<div>" + ctx.text() + ":" + dim.left + "," + dim.top + "</div>"
-    );
-    positions.push(dim);
-    ctx.animate(
-      {
-        left: dim.left,
-        top: dim.top
-      },
-      "slow"
-    );
-  });
-}); */
 
     $t.css({
       top: randomTopPos,
       left: randomLeftPos,
-
+      // filter: "blur(" + blur / zIndex + "px)",
       /* "url(" + link + ")" */
       background: color,
       width: size,
-      height: size //transform: "scale(" + scale + ")"
-    });
+      height: size,
+      //transform: "scale(" + 1.5 + ")",
+      zIndex: zIndex
+    }); // assign all values to css
   });
 });
+
+// call functions
+divMaker();
+manipulations();
